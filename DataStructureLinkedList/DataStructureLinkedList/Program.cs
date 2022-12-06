ae9d856bcba16ac5bb0de8a4bd8959b1680faea3
Rajesh kumar <rajeshbabu656656@gmail.com>
2022-12-07 00:01:24 +0530

    Appending LinkedList

--- DataStructureLinkedList/DataStructureLinkedList/Program.cs
+++ DataStructureLinkedList/DataStructureLinkedList/Program.cs
@@ -9,9 +9,10 @@ namespace DataStructure
             Console.WriteLine("******************* WELCOME TO DATASTRUCTURE LINKEDLIST PROGRAM ***********************");
 
             LinkedList l_Object = new LinkedList();
-            l_Object.add(70);
-            l_Object.add(30);
             l_Object.add(56);
+            l_Object.add(30);
+            l_Object.add(70);
+            l_Object.InsertAtParticularPosition(2, 30);
             l_Object.display();
 
         }
