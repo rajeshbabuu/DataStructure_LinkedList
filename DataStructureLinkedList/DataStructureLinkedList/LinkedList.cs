c67ce53874848814c8a4183c1ada40b61292d250
Rajesh kumar <rajeshbabu656656@gmail.com>
2022-12-06 23:39:30 +0530

    Create Own Linked List and adding element

---
+++ DataStructureLinkedList/DataStructureLinkedList/LinkedList.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructure
+{
+    internal class LinkedList
+    {
+        internal Node head;
+        public void add(int data)
+        {
+            Node node = new Node(data);
+            if (head == null)
+            {
+                head = node;
+            }
+            else
+            {
+                Node temp = head;
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = node;
+            }
+            Console.WriteLine("Insertes element = " + node.data);
+        }
+
+        public void display()
+        {
+            Node temp = this.head;
+            if (temp == null)
+            {
+                Console.WriteLine("Linked List is Empty");
+                return;
+            }
+            while (temp != null)
+            {
+                Console.WriteLine(temp.data + " ");
+                temp = temp.next;
+            }
+        }
+    }
+}
---
+++ DataStructureLinkedList/DataStructureLinkedList/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace DataStructure
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("******************* WELCOME TO DATASTRUCTURE LINKEDLIST PROGRAM ***********************");
+
+            LinkedList l_Object = new LinkedList();
+            l_Object.add(56);
+            l_Object.add(30);
+            l_Object.add(70);
+            l_Object.display();
+
+        }
+    }
+}
